7e2b82eba3c7dcb82671449ff97ab78b3f4eb9cf
Angel Mena Coudin <kratosmorze@gmail.com>
2025-09-03 13:54:44 -0600

    Agregar archivos de proyecto.

---
+++ backend-lab-C34789/backend-lab-C34789/Controllers/CountryController.cs
@@ -0,0 +1,23 @@
+﻿using backend_lab_C34789.Models;
+using backend_lab_C34789.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_lab_C34789.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase {
+        private readonly CountryService countryService;
+
+        public CountryController()
+        {
+            countryService = new CountryService();
+        }
+
+        [HttpGet]
+        public List<CountryModel> Get() {
+            var paises = countryService.GetCountries();
+            return paises;
+        }
+    }
+}
---
+++ backend-lab-C34789/backend-lab-C34789/Services/CountryService.cs
@@ -0,0 +1,20 @@
+﻿using backend_lab_C34789.Models;
+using backend_lab_C34789.Repositories;
+
+namespace backend_lab_C34789.Services
+{
+    public class CountryService
+    {
+        private readonly CountryRepository countryRepository;
+        public CountryService()
+        {
+            countryRepository = new CountryRepository();
+        }
+
+        public List<CountryModel> GetCountries()
+        {
+            // Add any missing business logic when it is neccesary
+            return countryRepository.GetCountries();
+        }
+    }
+}
