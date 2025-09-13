789b0e08cf01bb1518f0932802d61b75300c8e70
Angel Mena Coudin <kratosmorze@gmail.com>
2025-09-13 01:52:49 -0600

    Agregar nuevo país a la base de datos. Laboratorio #5

--- backend-lab-C34789/backend-lab-C34789/Repositories/CountryRepository.cs
+++ backend-lab-C34789/backend-lab-C34789/Repositories/CountryRepository.cs
@@ -5,20 +5,33 @@ using Dapper;
 
 namespace backend_lab_C34789.Repositories
 {
-    public class CountryRepository
+  public class CountryRepository
+  {
+    private readonly string _connectionString;
+    public CountryRepository()
     {
-        private readonly string _connectionString;
-        public CountryRepository()
-        {
-            var builder = WebApplication.CreateBuilder();
-            _connectionString = builder.Configuration.GetConnectionString("CountryContext");
-        }
-
-        public List<CountryModel> GetCountries()
-        {
-            using var connection = new SqlConnection( _connectionString );
-            string query = "SELECT * FROM dbo.Country";
-            return connection.Query<CountryModel>(query).ToList();
-        }
+      var builder = WebApplication.CreateBuilder();
+      _connectionString = builder.Configuration.GetConnectionString("CountryContext");
     }
+
+    public List<CountryModel> GetCountries()
+    {
+      using var connection = new SqlConnection( _connectionString );
+      string query = "SELECT * FROM dbo.Country";
+      return connection.Query<CountryModel>(query).ToList();
+    }
+
+    public bool CreateCountry(CountryModel country)
+    {
+      using var connection = new SqlConnection(_connectionString);
+      var query = @"INSERT INTO [dbo].[Country] ([Name],[Language],[Continent])
+          VALUES(@Name,@Language,@Continent)";
+      var affectedRows = connection.Execute(query, new {
+            Name = country.Name,
+            Language = country.Language,
+            Continent = country.Continent
+          });
+      return affectedRows >= 1;
+    }
+  }
 }
--- backend-lab-C34789/backend-lab-C34789/Services/CountryService.cs
+++ backend-lab-C34789/backend-lab-C34789/Services/CountryService.cs
@@ -3,18 +3,36 @@ using backend_lab_C34789.Repositories;
 
 namespace backend_lab_C34789.Services
 {
-    public class CountryService
+  public class CountryService
+  {
+    private readonly CountryRepository countryRepository;
+    public CountryService()
     {
-        private readonly CountryRepository countryRepository;
-        public CountryService()
-        {
-            countryRepository = new CountryRepository();
-        }
-
-        public List<CountryModel> GetCountries()
-        {
-            // Add any missing business logic when it is neccesary
-            return countryRepository.GetCountries();
-        }
+      countryRepository = new CountryRepository();
     }
+
+    public List<CountryModel> GetCountries()
+    {
+      // Add any missing business logic when it is neccesary
+      return countryRepository.GetCountries();
+    }
+
+    public string CreateCountry(CountryModel country)
+    {
+      var result = string.Empty;
+      try
+      {
+        var isCreated = countryRepository.CreateCountry(country);
+        if (!isCreated)
+        {
+          result = "Error al crear el país";
+        }
+      } catch (Exception)
+      {
+        result = "Error creando país";
+      }
+
+      return result;
+    }
+  }
 }
